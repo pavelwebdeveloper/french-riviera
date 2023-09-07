aebe657be339725d7567fa8431164273cdd728e4
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2023-09-07 12:01:39 -0600

    ETIMEDOUT error
    fixing ETIMEDOUT error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 //require('dotenv').config();
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 8080
+//const PORT = process.env.PORT || 8080
 const app = express();
 //const connectionString = process.env.DATABASE_URL;
 //var db = require('./db/dbConnection');
