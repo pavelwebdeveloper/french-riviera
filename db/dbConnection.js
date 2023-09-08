e5dbfa95f39f2dc61b587829bd550a7d1a562ecb
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2023-09-08 12:44:05 -0600

    db connection
    updated db connection

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -27,6 +27,7 @@ const mysql = require('mysql2');
 
 	connection = mysql.createConnection({
 	  host: process.env.MYSQLHOST,
+	  port: process.env.MYSQLPORT,
 	  user: process.env.MYSQLUSER,
 	  password: process.env.MYSQLPASSWORD,
 	  database: process.env.MYSQLDATABASE
